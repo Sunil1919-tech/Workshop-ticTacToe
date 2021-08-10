e3d25b5a55d56d20e33a42ef7a7d1dc505613a0e
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-10 14:49:16 +0530

    Modified: Creating the TictacToe game program

--- src/bridgeLabs/ticTacToeGame.java
+++ src/bridgeLabs/ticTacToeGame.java
@@ -1,28 +1,25 @@
 package bridgeLabs;
 
+/*
+ *Creating an Empty spaces for Tic Tac Toe Game
+ */
 public class ticTacToeGame {
-    /*
-     * Creating Board Method
-     */
+
+    //Creating Board Method
     public static char[] createBoard() {
-        /*
-         * Adding size to the Array
-         */
+        // Adding size of an  Array
         char[] board = new char[10];
 
-        /*
-        *Assigning empty space for each value
-        *neglecting zero index from board
-        * using for Loop condition
-       */
-
-        for(int i = 1;i < board.length; i++) {
-            board[i]= ' ';}
+        //Assigning empty space for each value
+        //neglecting zero index from board
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
         return board;
     }
-
+    //calling main method
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic Tac Toe Game");
-        char[] board=createBoard();
+        char[] board = createBoard();
     }
 }
